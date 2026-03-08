0b5d0a288e14ae6a313f54ee285badd585e6042f
shuchitripathi472-png <shuchitripathi472@gmail.com>
2026-03-08 22:08:38 +0530

    Add SQL queries for employee and department aggregation
    Added SQL queries for employee and department analysis.

--- Aggregation in SQL Anawers.sql
+++ Aggregation in SQL Anawers.sql
@@ -72,6 +72,69 @@ FROM Country
 GROUP BY Continent
 HAVING SUM(Population) > 200000000
 ORDER BY AvgLifeExpectancy DESC
+
+       --Q11--
+SELECT name
+FROM employee
+WHERE emp_id NOT IN (
+SELECT emp_id
+FROM sales
+);
+--or --
+SELECT name
+FROM employee e
+WHERE NOT EXISTS (
+SELECT 1
+FROM sales s
+WHERE e.emp_id = s.emp_id
+);
+
+--Q12--
+SELECT department_name
+FROM department
+WHERE department_id IN (
+    SELECT department_id
+    FROM employee
+    GROUP BY department_id
+    HAVING AVG(salary) > 55000
+);
+
+--Q13--
+SELECT department_name
+FROM department
+WHERE department_id IN (
+    SELECT department_id
+    FROM employee
+    WHERE emp_id IN (
+	SELECT emp_id
+	FROM sales
+	GROUP BY emp_id
+	HAVING SUM(sale_amount) > 10000)
+);
+
+--Q14--
+SELECT name
+FROM employee
+WHERE emp_id = (
+SELECT emp_id
+FROM sales
+WHERE sale_amount = (
+SELECT MAX(sale_amount)
+FROM sales
+WHERE sale_amount < (
+SELECT MAX(sale_amount)
+FROM sales))
+);
+
+--Q15--
+SELECT name
+FROM employee
+WHERE salary > (
+    SELECT MAX(sale_amount)
+    FROM sales
+);
+
+
 LIMIT 3;
 
 
